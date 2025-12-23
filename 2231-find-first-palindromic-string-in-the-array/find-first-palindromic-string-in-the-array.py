725eb0605be71cc85c769ba8768041b23a2925a3
Ram brahma reddy Yennam <118567959+RamBramhaReddy@users.noreply.github.com>
2025-12-23 12:54:17 +0530

    Time: 6 ms (38.17%) | Memory: 17.6 MB (97.18%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        n = len(words)
+        for i in range(n):
+            if words[i] == words[i][::-1]:
+                return words[i]
+        return ""
